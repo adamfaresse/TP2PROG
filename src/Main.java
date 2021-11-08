bb03e0b98f77970505ce46677db4acfed8aaf238
f18010428 <adam.faresse@etu.univ-amu.fr>
2021-11-08 13:59:29 +0100

    NEw

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        long startTime = System.currentTimeMillis();
         Dico dico = new Dico("/amuhome/f18010428/Bureau/S4/Algo2/TP/TPAlgo2/dico.txt");
         Trigramme trigramme = new Trigramme("<algorithmique>");
         //System.out.println(dico.dico);
@@ -13,16 +14,21 @@ public class Main {
         System.out.println(algo.calculate());
         System.out.println(trigramme.trigramme);
         List<String >L = trigramme.ListeL(dico);
+
         //System.out.println(L);
         HashMap<String,Integer> Occu = trigramme.OccurenceDansL(L);
 
         //System.out.println(Occu);
-        List<String>Ltrie = trigramme.ListeTriage(L);
+        //List<String>Ltrie = trigramme.ListeTriage(L);
         //System.out.println(Ltrie);
-        List<String> OccuMax = trigramme.ListeOccuMax(Occu,Ltrie);
+        List<String> OccuMax = trigramme.ListeOccuMax(Occu);
+
+        //List<String> OccuMax = trigramme.ListeOccuMax(Occu,Ltrie);
         System.out.println(OccuMax);
         List<String> CinqMeilleur = trigramme.DistanceList(OccuMax);
         System.out.println(CinqMeilleur);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total execution time: " + ((endTime-startTime)/1000) + "s");
 
     }
 }
--- src/Trigramme.java
+++ src/Trigramme.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Trigramme {
     public List<String>  trigramme = new ArrayList<String>();
     public String GrandM ;
+    public List<String> ListTrie = new ArrayList<>();
     public Trigramme(String mot){
         for (int i = 0; i <mot.length()-2 ; i++) {
                 this.trigramme.add(mot.charAt(i) + Character.toString(mot.charAt(i + 1)) + mot.charAt(i + 2));
@@ -24,10 +25,18 @@ public class Trigramme {
         return L;
     }
 
-    public HashMap<String, Integer> OccurenceDansL(List<String> liste){
+    public HashMap<String, Integer>  OccurenceDansL(List<String> liste){
         HashMap<String,Integer> ListeLEtOcc = new HashMap<>();
+        for(String mot : liste){
+            int conteur = 0;
+            if(!ListeLEtOcc.containsKey(mot)){
+                this.ListTrie.add(mot);
+                conteur = Collections.frequency(liste,mot);
+                ListeLEtOcc.put(mot,conteur);
+            }
 
-        for (String mot1 : liste){
+        }
+        /*for (String mot1 : liste){
             int conteur = 0;
             if(!ListeLEtOcc.containsKey(mot1)){
                 for (String mot2 : liste){
@@ -37,14 +46,12 @@ public class Trigramme {
                 }
                 ListeLEtOcc.put(mot1,conteur);
             }
-
-
-        }
+        }*/
         return ListeLEtOcc;
     }
 
 
-    public List<String> ListeTriage(List<String>listeNonTrie){
+    /*public List<String> ListeTriage(List<String>listeNonTrie){
         List<String > listeTrie = new ArrayList<>();
         for (String mot: listeNonTrie){
             if(!listeTrie.contains(mot)){
@@ -52,14 +59,14 @@ public class Trigramme {
             }
         }
         return listeTrie;
-    }
-    public List<String> ListeOccuMax(HashMap<String,Integer> listeMap,List<String> listeTrie){
+    }*/
+    public List<String> ListeOccuMax(HashMap<String,Integer> listeMap){
         LinkedList<String> listeMax = new LinkedList<>();
         int valeur= 1;
         boolean bool = false;
         while(!bool){
             bool = true;
-            for (String mot : listeTrie){
+            for (String mot : this.ListTrie){
                 listeMap.get(mot);
                 if (valeur==listeMap.get(mot)){
                     if(listeMax.size()==100){
@@ -74,6 +81,27 @@ public class Trigramme {
         }
         return listeMax;
     }
+    /*public List<String> ListeOccuMax(HashMap<String,Integer> listeMap,List<String> ListTrie){
+        LinkedList<String> listeMax = new LinkedList<>();
+        int valeur= 1;
+        boolean bool = false;
+        while(!bool){
+            bool = true;
+            for (String mot : ListTrie){
+                listeMap.get(mot);
+                if (valeur==listeMap.get(mot)){
+                    if(listeMax.size()==100){
+                        listeMax.removeFirst();
+                    }
+                    listeMax.add(mot);
+                    bool = false;
+                }
+            }
+            valeur++;
+
+        }
+        return listeMax;
+    }*/
 
     public List<String> DistanceList(List<String> listeMax){
         LinkedList<String> ListeMeilleurMot = new LinkedList<>();
